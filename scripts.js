bc67586cfb8dcc896549f8cb71f8ae5e17432dc4
Niveena KJ <niveenajoseph@gmail.com>
2024-10-20 22:36:41 +0530

    Website creation

---
+++ scripts.js
@@ -0,0 +1,46 @@
+/*!
+* Start Bootstrap - Resume v7.0.6 (https://startbootstrap.com/theme/resume)
+* Copyright 2013-2023 Start Bootstrap
+* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-resume/blob/master/LICENSE)
+*/
+//
+// Scripts
+// 
+
+window.addEventListener('DOMContentLoaded', event => {
+
+    // Activate Bootstrap scrollspy on the main nav element
+    const sideNav = document.body.querySelector('#sideNav');
+    if (sideNav) {
+        new bootstrap.ScrollSpy(document.body, {
+            target: '#sideNav',
+            rootMargin: '0px 0px -40%',
+        });
+    };
+
+    // Collapse responsive navbar when toggler is visible
+    const navbarToggler = document.body.querySelector('.navbar-toggler');
+    const responsiveNavItems = [].slice.call(
+        document.querySelectorAll('#navbarResponsive .nav-link')
+    );
+    responsiveNavItems.map(function (responsiveNavItem) {
+        responsiveNavItem.addEventListener('click', () => {
+            if (window.getComputedStyle(navbarToggler).display !== 'none') {
+                navbarToggler.click();
+            }
+        });
+    });
+
+});
+
+//  Function to toggle the visibility of a specific element and hide others
+function toggleContact(paragraphid) {
+    var paragraph = document.getElementById(paragraphid);
+    var allElements = document.querySelectorAll('.hidden-content');
+
+    // Hide all other elements
+    allElements.forEach(function (el) {
+        el.style.display = 'none';
+    });
+    paragraph.style.display = (paragraph.style.display === "" || paragraph.style.display === "none") ? "block" : "none";
+}
